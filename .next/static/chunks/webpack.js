b9b0ed3812afa3bd3cc69b7401fdfac10ebfe244
Samet Samyeli <ruzgarsamet@gmail.com>
2023-07-17 11:26:16 +0300

    NXT: Search component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "290b18fdf10aaad3"; }
+/******/ 		__webpack_require__.h = function() { return "dfb1c8fec24a970b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,45 +1,50 @@
+import { useState } from 'react';
 import Logo from '../../ui/Logo';
-import {FaUserAlt, FaShoppingCart, FaSearch} from 'react-icons/fa';
-
+import { FaUserAlt, FaShoppingCart, FaSearch } from 'react-icons/fa';
+import Search from '../../ui/Search';
 
 const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false);
   return (
     <div className="h-[5.5rem] bg-secondary justify-between">
       <div className="container flex text-white mx-auto h-full items-center">
         <div>
           <Logo />
         </div>
-        <nav className='flex justify-center w-full '>
-          <ul className='flex'>
-            <li className='px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer'>
-              <a href="" className='text'>Home</a>
+        <nav className="flex justify-center w-full ">
+          <ul className="flex">
+            <li className="px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer">
+              <a href="" className="text">
+                Home
+              </a>
             </li>
-            <li className='px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer'>
+            <li className="px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer">
               <a href="">About</a>
             </li>
-            <li className='px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer'>
+            <li className="px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer">
               <a href="">Menu</a>
             </li>
-            <li className='px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer'>
+            <li className="px-[5px] py-[20px] uppercase hover:text-primary cursor-pointer">
               <a href="">Book Table</a>
             </li>
           </ul>
         </nav>
-        <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <a href="#">
-            <FaUserAlt className='hover:text-primary transition-all' />
+            <FaUserAlt className="hover:text-primary transition-all" />
           </a>
           <a href="#">
-            <FaShoppingCart className='hover:text-primary transition-all' />
+            <FaShoppingCart className="hover:text-primary transition-all" />
           </a>
+          <button onClick={() => setIsSearchModal(true)}>
+            <FaSearch className="hover:text-primary transition-all" />
+          </button>
           <a href="#">
-            <FaSearch className='hover:text-primary transition-all' />
-          </a>
-          <a href="#">
-            <button className='btn-primary w-[160px]'>Order Online</button>
+            <button className="btn-primary w-[160px]">Order Online</button>
           </a>
         </div>
       </div>
+      {isSearchModal && <Search setIsSearchModal={setIsSearchModal} />}
     </div>
   );
 };
